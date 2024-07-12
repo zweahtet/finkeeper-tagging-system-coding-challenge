15dae65ade5369916afd439ec4db6e7768559928
Zwea Htet <zhtet5@berkeley.edu>
2024-07-11 17:48:11 -0700

    updated test cases

--- blog_project/blog/tests.py
+++ blog_project/blog/tests.py
@@ -6,36 +6,43 @@ from .models import Post, Tag
 
 class TaggingSystemTests(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username="testuser", password="12345")
         self.post = Post.objects.create(
-            title="Test Post", content="Content", author=self.user
+            title="Test Post", content="Test Content", author="Test Author"
         )
+        self.tag1 = Tag.objects.create(name="tag1")
+        self.tag2 = Tag.objects.create(name="tag2")
+        self.post.tags.add(self.tag1, self.tag2)
 
     def test_tag_creation(self):
-        tag = Tag.objects.create(name="testtag")
-        self.assertEqual(tag.name, "testtag")
+        tag = Tag.objects.create(name="newtag")
+        self.assertEqual(Tag.objects.count(), 3)
 
     def test_tag_association(self):
-        tag = Tag.objects.create(name="testtag")
-        self.post.tags.add(tag)
-        self.assertIn(tag, self.post.tags.all())
+        self.assertEqual(self.post.tags.count(), 2)
 
-    def test_tag_search(self):
-        tag1 = Tag.objects.create(name="tag1")
-        tag2 = Tag.objects.create(name="tag2")
-        self.post.tags.add(tag1, tag2)
+    def test_search_by_tag(self):
+        response = self.client.get(reverse("search_posts") + "?q=tag1&filter_type=any")
+        self.assertContains(response, "Test Post")
 
+    def test_filter_all_tags(self):
         response = self.client.get(
             reverse("search_posts") + "?q=tag1,tag2&filter_type=all"
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.post.title)
+        self.assertContains(response, "Test Post")
 
-        response = self.client.get(reverse("search_posts") + "?q=tag1&filter_type=any")
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, self.post.title)
+    def test_filter_any_tags(self):
+        response = self.client.get(
+            reverse("search_posts") + "?q=tag1,nonexistent&filter_type=any"
+        )
+        self.assertContains(response, "Test Post")
 
-    def test_tag_uniqueness(self):
-        Tag.objects.create(name="unique")
-        with self.assertRaises(Exception):
-            Tag.objects.create(name="unique")
+    def test_filter_specific_combination(self):
+        response = self.client.get(
+            reverse("search_posts") + "?q=tag1,tag2&filter_type=specific"
+        )
+        self.assertContains(response, "Test Post")
+
+    def test_popular_tags(self):
+        response = self.client.get(reverse("search_posts"))
+        self.assertContains(response, "tag1")
+        self.assertContains(response, "tag2")
